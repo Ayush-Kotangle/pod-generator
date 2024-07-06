cbf2874679873d4ff39a53b21b3e7674f767f3a7
Ayush <ayushkotangle825@gmail.com>
2024-07-06 22:57:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+python3 /user/bin/feed.py
 
 git add -A && commit -m "Update Feed"
 
